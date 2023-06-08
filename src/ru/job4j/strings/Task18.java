caab62556bd7f2b5c5afc93dc35eac752961fafc
alkrivosheev <alkrivosheev@gmail.com>
2023-06-08 10:09:02 +0300

    Task18

--- src/ru/job4j/strings/Task18.java
+++ src/ru/job4j/strings/Task18.java
@@ -1,4 +1,20 @@
 package ru.job4j.strings;
 
+import java.util.Scanner;
+
 public class Task18 {
+    public static void main(String[] args) {
+        double p;
+        double l;
+        double s;
+        Scanner input = new Scanner(System.in);
+        double a = input.nextDouble();
+        double r = input.nextDouble();
+        p = a * 4;
+        l = 2 * Math.PI * r;
+        s = Math.PI * Math.pow(r, 2);
+        System.out.println(String.format("%.1f", p));
+        System.out.println(String.format("%.1f", l));
+        System.out.println(String.format("%.1f", s));
+    }
 }
