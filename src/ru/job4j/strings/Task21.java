b2eb1b4fc8538ba7691055d27e9876a9b744824e
alkrivosheev <alkrivosheev@gmail.com>
2023-06-08 10:24:34 +0300

    Task21

---
+++ src/ru/job4j/strings/Task21.java
@@ -0,0 +1,12 @@
+package ru.job4j.strings;
+
+public class Task21 {
+    public static void main(String[] args) {
+        String fullName = "Фамилия Имя Отчество";
+        String phoneNumber = "+7(938) 123-45-67";
+        System.out.println("************************");
+        System.out.println("* " + fullName + " *");
+        System.out.println("* " + phoneNumber + "    *");
+        System.out.print("************************");
+    }
+}
