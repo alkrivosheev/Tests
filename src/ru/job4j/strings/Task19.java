5ea30fa52d721715edd32a3386e2ef4265b0f8ca
alkrivosheev <alkrivosheev@gmail.com>
2023-06-08 10:18:44 +0300

    Task20

---
+++ src/ru/job4j/strings/Task19.java
@@ -0,0 +1,10 @@
+package ru.job4j.strings;
+
+public class Task19 {
+    public static void main(String[] args) {
+        int x = 9;
+        System.out.println("Значение x равно " + x);
+        System.out.println("Значение x^2 равно " + (int)Math.pow(x, 2));
+        // YOUR CODE
+    }
+}
---
+++ src/ru/job4j/strings/Task20.java
@@ -0,0 +1,14 @@
+package ru.job4j.strings;
+
+public class Task20 {
+    public static void print(int x, int y) {
+        System.out.println("Переменные: " + x + " " + y);
+        System.out.println("Сумма: " + (x + y));
+        System.out.println("Произведение: " + x * y);
+        //...Ваш код...;
+    }
+
+    public static void main(String[] args) {
+        print(5, 5);
+    }
+}
