48479fced67df430c31d5e2c1998d870ba482a53
alkrivosheev <alkrivosheev@gmail.com>
2023-08-14 07:35:53 +0300

    Loop

---
+++ src/ru/job4j/loop/Print0ToN.java
@@ -0,0 +1,13 @@
+package ru.job4j.loop;
+
+public class Print0ToN {
+    public static void out(int n) {
+        for (int i = 0; i < n; i++) {
+            System.out.println(i);
+        }
+    }
+
+    public static void main(String[] args) {
+        out(9);
+    }
+}
