31ce747c6334746131ec5cd9678463c214bc2cf2
alkrivosheev <alkrivosheev@gmail.com>
2024-07-25 11:47:42 +0300

    test

--- src/ru/job4j/array/NonSquareArray.java
+++ src/ru/job4j/array/NonSquareArray.java
@@ -1,7 +1,7 @@
 package ru.job4j.array;
 
 public class NonSquareArray {
-    public static void main(String[] args) {
+    public static void main(String[] args ) {
         int[][] array = new int[][]{{1}, {2, 3}, {4, 5, 6}};
         for (int i = 0; i < array.length; i++) {
             System.out.println(array[i].length);
